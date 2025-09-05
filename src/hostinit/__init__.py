0aa76752fb2c301f2ed4379d74bde11230027e17
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2025-09-05 12:33:08 -0500

    first commit

--- src/hostinit/__init__.py
+++ src/hostinit/__init__.py
@@ -1,9 +1,8 @@
 import os
-from pkg_resources import parse_version
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-__version__ = "1.0.59"
-VERSION = parse_version(__version__)
+__version__ = "1.0.0"
+VERSION = __version__
 
 
 def get_playbook_file(file):
